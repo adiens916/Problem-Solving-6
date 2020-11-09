21a17eeca5ee4f5d5273a22158cd8de59a5173a2
adiens916 <adiens916@naver.com>
2020-11-10 00:08:37 +0900

    AdminModel에서 DB 연결 부분 수정 (템플릿)
    con 부분은 같지만 불러오는 방식이 달라짐.
    & 미완성 부분 포함: View의 버튼들을
    Controller를 통해 가져오는 작업 필요.

--- RentcarDatabase/src/model/AdminModel.java
+++ RentcarDatabase/src/model/AdminModel.java
@@ -1,21 +1,19 @@
-package Model;
+package model;
 
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-import View.AdminView;
-import View.dataClass.AdminInfo;
+import view.AdminView;
+import view.dataClass.AdminInfo;
 
 public class AdminModel {
 	// ConnectToDatabase 클래스에서 데이터베이스와 연결해주는 메소드(conDB)를 가져온다.
-	public static ConnectToDatabase connection = new ConnectToDatabase();
-	public AdminModel() {		
-		 connection.conDB(); //DB연결
-	 }
+	private Connection con = DatabaseConnector.connection;
 	//AdminView에서 입력받은 데이터를 가져오기 위한 객체
 	 AdminView adminInfo = new AdminView();
 	 Statement stmt;
@@ -24,13 +22,13 @@ public class AdminModel {
 	 StringBuilder results = new StringBuilder();
 	 //데이터베이스에 Insert후 성공여부를 알기위한 변수
 	 int result;
-	 
-	 
+
+
 	 //관리자 페이지 오픈과 동시에 고객이 캠핑카를 반환할 때 캠핑카의 상태를 출력하기 위한 메소드
 	 public String printCampincarreturn(){
 		 try {
 			results.append("앞쪽 \t 오른쪽 \t 왼쪽 \t 뒤쪽 \t 수리여부 \t 캠핑카ID \t 고유대여ID\n");
-			stmt = connection.con.createStatement();
+			stmt = con.createStatement();
 			String query="SELECT * FROM campingcar_return;";
 			rs = stmt.executeQuery(query);
              while(rs.next()) {
@@ -47,7 +45,7 @@ public class AdminModel {
 	public String printGarageResult(){
 		try {
 			results.append("차고지ID \t 카센터이름 \t 주소 \t 번호 \t 매니저이름 \t 이메일주소\n");
-			stmt = connection.con.createStatement();
+			stmt = con.createStatement();
 			String query="SELECT * FROM garage;";
 			rs = stmt.executeQuery(query);
              while(rs.next()) {
@@ -82,33 +80,33 @@ public class AdminModel {
 		 * 1. "정비소로보내기"버튼 grgbtn 객체
 		*/
 		try {
-			stmt = connection.con.createStatement();
+			stmt = con.createStatement();
 			String query = "select fix, custom_rent_list_id from campingcar_return where campingcar_list_id='"+admin.cpcid+"'";
 			rs = stmt.executeQuery(query);
 			if(rs.next()) {
 				admin.fixtest = rs.getString(1);
 				admin.rent_id = rs.getString(2);
 			}
-			
-			
-			stmt = connection.con.createStatement();
+
+
+			stmt = con.createStatement();
 			query = "select c_license_id,campingcar_company_id from customer_rent_old_list where rent_id='"+admin.rent_id+"';";
 			rs = stmt.executeQuery(query);
 			if(rs.next()) {
 				admin.lisenceid = rs.getString(1);
 				admin.companyid = rs.getString(2);
 			}
-			
-			
+
+
 			if(admin.fixtest.equals("1")) { //수리할때
-				stmt = connection.con.createStatement();
+				stmt = con.createStatement();
 				query = "insert into repair_list(r_log,r_date,r_price,r_due_date,r_other_repair,customer_license_id,campingcar_rent_company_id,garage_id,campingcar_list_id) "
 						+ "values('"+admin.r_log+"','"+admin.r_date+"','"+admin.r_price+"','"+admin.r_duedate+"'"
 						+ ",'"+admin.r_otherinfo+"','"+admin.lisenceid+"','"+admin.companyid+"','"+admin.grg_id+"'"
 						+ ",'"+admin.cpcid+"');";
 				int result = stmt.executeUpdate(query);
-				
-				stmt = connection.con.createStatement();
+
+				stmt = con.createStatement();
 				query="DELETE FROM campingcar_return WHERE campingcar_list_id = '"+admin.cpcid+"'";
 				int result1 = stmt.executeUpdate(query);
 				if(result == 1 && result1==1) {
@@ -120,8 +118,8 @@ public class AdminModel {
 			}else if(admin.fixtest.equals("0")) {//수리필요없을때
 				//JOptionPane.showMessageDialog(grgbtn, "수리할필요없습니다. 반환하세요.");
 			}
-			
-			
+
+
 		}catch(Exception e1) {
 			if(admin.r_log.length()==0||admin.r_date.length()==0||admin.r_price.length()==0||
 				admin.r_duedate.length()==0||admin.r_otherinfo.length()==0||admin.grg_id.length()==0||
@@ -130,7 +128,7 @@ public class AdminModel {
 			}
 		}
 	}
-	
+
 	/*
 	 * 정비소로 보내지 않고 반환하기
 	 * - 먼저 수리여부 변수가 1인지 0인지 판단(0이면 반환)
@@ -143,13 +141,13 @@ public class AdminModel {
 		*/
 		String cpcid = admin.torepair;
 		try {
-		stmt = connection.con.createStatement();
+		stmt = con.createStatement();
 		String fixtest = null;
 		String query = "select fix from campingcar_return where campingcar_list_id='"+cpcid+"'";
 		rs = stmt.executeQuery(query);
-		if(rs.next()) {fixtest = rs.getString(1);}		
-		
-		
+		if(rs.next()) {fixtest = rs.getString(1);}
+
+
 		query="select * from campingcar_list where campingcar_list_id='"+cpcid+"'";
 		rs = stmt.executeQuery(query);
 		String cc_name=null;
@@ -172,9 +170,9 @@ public class AdminModel {
         	 cc_regitst_date =rs.getString(9);
         	 campingcar_rent_company_id =rs.getString(10);
          }
-         
+
         	  if(fixtest.equals("1")) {
-        		 // JOptionPane.showMessageDialog(toreturnbtn, "수리가필요한 캠핑카입니다."); 
+        		 // JOptionPane.showMessageDialog(toreturnbtn, "수리가필요한 캠핑카입니다.");
         	  }else if(fixtest.equals("0")) {//반환하기!
         		  query="insert into rentcar_list values"
         				  + "('"+cpcid+"','"+cc_name+"','"+cc_number+"','"+cc_sits+"',"
@@ -184,9 +182,9 @@ public class AdminModel {
   	 			result = stmt.executeUpdate(query);
   				query="DELETE FROM campingcar_return WHERE campingcar_list_id = '"+cpcid+"'";
   				result = stmt.executeUpdate(query);
-  				
-  				//JOptionPane.showMessageDialog(toreturnbtn, "반환 완료!"); 
-        		//returnresult();	            		  
+
+  				//JOptionPane.showMessageDialog(toreturnbtn, "반환 완료!");
+        		//returnresult();
         	  }
 		}catch(Exception e1) {
 			if(cpcid.length()==0) {
@@ -194,19 +192,19 @@ public class AdminModel {
 			}
 		}
 	}
-	
+
 	//검색 1
 	public String Search1() {
 		results.append("검색1 결과\n");
 		try {
-		stmt = connection.con.createStatement();
-		String query = "select c_name\r\n" + 
-				"from (select r_price,customer_license_id\r\n" + 
-				"   from repair_list\r\n" + 
-				"    where r_price >=10) rp,\r\n" + 
-				"    customer cs\r\n" + 
-				"where cs.license_id=rp.customer_license_id\r\n" + 
-				"group by cs.c_name;\r\n" + 
+		stmt = con.createStatement();
+		String query = "select c_name\r\n" +
+				"from (select r_price,customer_license_id\r\n" +
+				"   from repair_list\r\n" +
+				"    where r_price >=10) rp,\r\n" +
+				"    customer cs\r\n" +
+				"where cs.license_id=rp.customer_license_id\r\n" +
+				"group by cs.c_name;\r\n" +
 				"";
 		rs = stmt.executeQuery(query);
 		String str =null;
@@ -222,21 +220,21 @@ public class AdminModel {
 		}catch(Exception e1) {
 			System.out.println(e1);
 		}
-		
+
 		return results.toString();
 	}
 	//검색 2
 	public String Search2() {
 		results.append("검색2 결과\n");
 		try {
-		stmt = connection.con.createStatement();
-		String query = "select c_name\r\n" + 
-				"FROM    (SELECT  cc_price,c_license_id\r\n" + 
-				"    FROM   customer_rent_list\r\n" + 
-				"    WHERE  cc_price >= 50) rl,\r\n" + 
-				"    customer cs\r\n" + 
-				"WHERE    cs.license_id=rl.c_license_id\r\n" + 
-				"GROUP BY cs.c_name;\r\n" + 
+		stmt = con.createStatement();
+		String query = "select c_name\r\n" +
+				"FROM    (SELECT  cc_price,c_license_id\r\n" +
+				"    FROM   customer_rent_list\r\n" +
+				"    WHERE  cc_price >= 50) rl,\r\n" +
+				"    customer cs\r\n" +
+				"WHERE    cs.license_id=rl.c_license_id\r\n" +
+				"GROUP BY cs.c_name;\r\n" +
 				"";
 		rs = stmt.executeQuery(query);
 		String str =null;
@@ -258,13 +256,13 @@ public class AdminModel {
 	public String Search3() {
 		results.append("검색3 결과\n");
 		try {
-		stmt = connection.con.createStatement();
-		String query = "select g_name\r\n" + 
-				"from (select cc_manufacture, campingcar_list_id\r\n" + 
-				"   from campingcar_list\r\n" + 
-				"   where cc_manufacture_year >=2000) cl, \r\n" + 
-				"    garage g\r\n" + 
-				"where g.garage_id = cl.campingcar_list_id\r\n" + 
+		stmt = con.createStatement();
+		String query = "select g_name\r\n" +
+				"from (select cc_manufacture, campingcar_list_id\r\n" +
+				"   from campingcar_list\r\n" +
+				"   where cc_manufacture_year >=2000) cl, \r\n" +
+				"    garage g\r\n" +
+				"where g.garage_id = cl.campingcar_list_id\r\n" +
 				"group by g.g_name;";
 		rs = stmt.executeQuery(query);
 		String str =null;
@@ -286,13 +284,13 @@ public class AdminModel {
 	public String Search4() {
 		results.append("검색4 결과\n");
 		try {
-		stmt = connection.con.createStatement();
-		String query = "select c_name\r\n" + 
-				"FROM    (SELECT  rent_time,c_license_id\r\n" + 
-				"    FROM   customer_rent_list\r\n" + 
-				"    WHERE  rent_time>=10) rl,\r\n" + 
-				"    customer cs\r\n" + 
-				"WHERE    cs.license_id=rl.c_license_id\r\n" + 
+		stmt = con.createStatement();
+		String query = "select c_name\r\n" +
+				"FROM    (SELECT  rent_time,c_license_id\r\n" +
+				"    FROM   customer_rent_list\r\n" +
+				"    WHERE  rent_time>=10) rl,\r\n" +
+				"    customer cs\r\n" +
+				"WHERE    cs.license_id=rl.c_license_id\r\n" +
 				"GROUP BY cs.c_name;";
 		rs = stmt.executeQuery(query);
 		String str =null;
