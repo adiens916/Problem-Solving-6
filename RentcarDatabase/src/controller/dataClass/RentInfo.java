5349a750f21e5f692e90bf0d549c547fb24f17e0
JamesKim <67713123+KimTae-Gyu@users.noreply.github.com>
2020-11-11 02:33:05 +0900

    고객 페이지(customer)의 대여 기능에 필요한 데이터 클래스 RentInfo

---
+++ RentcarDatabase/src/controller/dataClass/RentInfo.java
@@ -0,0 +1,21 @@
+package Controller.dataClass;
+
+public class RentInfo {
+	public String campingCarID;
+	public String license;
+	public String rentStartDate;
+	public String rentPeriod;
+	public String rentEndDate;
+	public String extraGoods;
+	public String extraGoodsPrice;
+	
+	public boolean isNull() {
+		return campingCarID.length() == 0 ||
+				license.length() == 0 ||
+				rentStartDate.length() == 0 ||
+				rentPeriod.length() == 0 ||
+				rentEndDate.length() == 0 ||
+				extraGoods.length() == 0 ||
+				extraGoodsPrice.length() == 0;
+	}
+}
