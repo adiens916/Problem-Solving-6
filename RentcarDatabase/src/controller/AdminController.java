380654f2b6da4476b4ca5494d9776ca0e1d76fb2
BaskBoomy <57517612+BaskBoomy@users.noreply.github.com>
2020-11-28 02:01:35 +0900

    Update AdminController.java

--- RentcarDatabase/src/controller/AdminController.java
+++ RentcarDatabase/src/controller/AdminController.java
@@ -1,27 +1,16 @@
 package controller;
 
 import java.util.ArrayList;
-import view.AdminView;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import controller.dataClass.AdminInfo;
 import controller.dataClass.CampingCarInfo;
 import controller.dataClass.Garage;
 import model.AdminModel;
-import model.CampingCarModel;
 
 public class AdminController {
 	AdminModel adminModel = new AdminModel();
-	AdminView adminView = AdminView.getInstance();
-
-	public static AdminController getInstance() {
-		return AdminControllerHolder.instance;
-	}
-
-	private static class AdminControllerHolder {
-		private static final AdminController instance = new AdminController();
-	}
-
-	public AdminController() {}
-
+	
 	public String printCampingcarList() {
 		String str = "캠핑카ID \t 차명 \t 차량번호 \t 승차인원수 \t 제조회사 \t 제조연도 \t 누적주행거리 \t 대여비용 \t캠핑카등록일자 \t 대여회사ID \n";
 		ArrayList<CampingCarInfo> campingCarList = adminModel.getCampingCarList();
@@ -41,10 +30,8 @@ public class AdminController {
 	}
 
 	public String printGarageList() {
-
 		String str = "차고지ID \t 카센터이름 \t 주소 \t 번호 \t 매니저이름 \t 이메일주소\n";
 		ArrayList<Garage> campingCarList = adminModel.getGarageList();
-		
 		for (Garage garage : campingCarList) {
 			str += garage.id + '\t' +
 					garage.name + '\t' +
@@ -53,34 +40,36 @@ public class AdminController {
 					garage.manager + '\t' +
 					garage.emailAddress+ '\n';
 		}
-		
 		return str;
 	}
 	
 	
 	
-	public int InsetGarageData(ArrayList<AdminInfo> adminData) {
-		return adminModel.InsetToGarage(adminData);
+	public void insetGarageData(ArrayList<AdminInfo> adminData) {
+		int result = adminModel.insetToGarage(adminData);
+		if(result ==1 ) {JOptionPane.showMessageDialog(null, "처리 완료");}
+		else if(result ==0 ) JOptionPane.showMessageDialog(null, "수리할필요없습니다. 반환하세요.");
+		else if(result ==2 ) JOptionPane.showMessageDialog(null, "빈칸을 모두 채워주세요");
 	}
-	public int ReturnToCampingCarData(String cpcid) {
-		return adminModel.ReturnToCampingcarList(cpcid);
+	
+	public void returnToCampingCarData(String cpcid) {
+		int result = adminModel.returnToCampingcarList(cpcid);
+		if(result==1) JOptionPane.showMessageDialog(null, "수리가필요한 캠핑카입니다."); 
+		else if(result==2) JOptionPane.showMessageDialog(null, "반환 완료!"); 
+		else if(result==3) JOptionPane.showMessageDialog(null, "캠핑카ID를 입력해주세요!");
+		
 	}
-	public void printSearch(int num) {
+	
+	public void printSearch(int num, JTextArea searchText) {
 		try {
 			 ArrayList<String> search = new ArrayList<>();
 			 search = adminModel.getSearch(num);
 			for(String i : search) {
-				adminView.searchText.append(i);
+				//adminView.searchText.append(i);
+				searchText.append(i);
 			}
 		}catch(Exception e1) {
 			e1.printStackTrace();
 		}
-	}
-	public void textFieldReset() {
-		adminModel.dataReset(adminView);
-	}
-
-	public void setVisible(boolean value) {
-		AdminView.getInstance().setVisible(value);
 	}
 }
