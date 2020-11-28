e3d0b543ae063525cc792b8fd581e34a46898f0b
hkmast <50509845+hkmast@users.noreply.github.com>
2020-11-28 17:54:41 +0900

    Add files via upload

--- RentcarDatabase/src/model/CampingCarModel.java
+++ RentcarDatabase/src/model/CampingCarModel.java
@@ -1,18 +1,91 @@
 package model;
 
-import java.sql.*;
-import java.util.ArrayList;
 import controller.dataClass.CampingCarInfo;
-import controller.dataClass.ResultState;
 
-public class CampingCarModel {
-    /* SQL 연결 */
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class CampingCarModel extends AbstractModel<CampingCarInfo>{
+
+	String getCreateQuery(CampingCarInfo campingCar) {
+        return "insert into campingcar_list(" +
+                "cc_name,cc_number,cc_sits,cc_manufacture,cc_manufacture_year," +
+                "cc_mileage,cc_rent_price,cc_regist_date,campingcar_rent_company_id)" +
+                " values('"+
+                campingCar.campingCarName+"','"+
+                campingCar.campingCarNumber+"','"+
+                campingCar.campingCarSits+"','"+
+                campingCar.campingCarManufacutre+"','"+
+                campingCar.campingCarManufactureYear+"','"+
+                campingCar.campingCarMileage+"','"+
+                campingCar.campingCarRentprice+"','"+
+                campingCar.campingCarRegitstdate +"','"+
+                campingCar.campingCarRentCompanyId+"')";
+    }
+	
+	
+	String getReadAllQuery() {
+        return "select * from campingcar_list";
+    }
+	
+    String getReadQuery(String id) {
+        return " select * from campingcar_list where campingcar_list_id='"+id+"';";
+    }
+    
+    String getUpdateQuery(CampingCarInfo campingCar) {
+        return "update campingcar_list set cc_name='"+campingCar.campingCarName+
+                "',cc_number='"+campingCar.campingCarNumber+
+                "',cc_sits='"+campingCar.campingCarSits+
+                "',cc_manufacture='"+campingCar.campingCarManufacutre+
+                "',cc_manufacture_year='"+campingCar.campingCarManufactureYear+
+                "',cc_mileage='"+campingCar.campingCarMileage+
+                "',cc_rent_price='"+campingCar.campingCarRentprice+
+                "',cc_regist_date='"+campingCar.campingCarRegitstdate +
+                "',campingcar_rent_company_id='"+campingCar.campingCarRentCompanyId+"'" +
+                " where campingcar_list_id='"+campingCar.campingCarId+"'";
+    }
+    
+    String getDeleteQuery(String id) {
+        return "DELETE FROM campingcar_list WHERE campingcar_list_id = '"+id+"'";
+    }
+	
+	
+    boolean isNullData(CampingCarInfo campingCar) {
+        return campingCar.isNull();
+    }
+
+    @Override
+    CampingCarInfo toInfoFromResultSet(ResultSet resultSet) throws SQLException{
+        CampingCarInfo campingCar = new CampingCarInfo();
+        campingCar.campingCarRentCompanyId = Integer.toString(resultSet.getInt(1));
+        campingCar.campingCarName = resultSet.getString(2);
+        campingCar.campingCarNumber = resultSet.getString(3);
+        campingCar.campingCarSits = resultSet.getString(4);
+        campingCar.campingCarManufacutre = resultSet.getString(5);
+        campingCar.campingCarManufactureYear = resultSet.getString(6);
+        campingCar.campingCarMileage = resultSet.getString(7);
+        campingCar.campingCarRentprice = resultSet.getString(8);
+        campingCar.campingCarRegitstdate = resultSet.getString(9);
+        campingCar.campingCarRentCompanyId = resultSet.getString(10);
+        return campingCar;
+    }
+}
+	
+	
+	
+	/*
+	
+	////////////////////////////////////////////////
+	// SQL 연결 
     private final Connection connection = DatabaseConnector.getConnection();
     private Statement statement;
     private ResultSet resultSet;
 
+    //
     public ResultState createCampingCar(CampingCarInfo campingCar) {
-        /* 컨트롤러를 통해 받아온 데이터를 모델에서 입력 */
+        // 컨트롤러를 통해 받아온 데이터를 모델에서 입력 
         try {
             statement = connection.createStatement();
             String query="insert into campingcar_list(" +
@@ -28,15 +101,15 @@ public class CampingCarModel {
                     campingCar.rentalFee+"','"+
                     campingCar.registryDate +"','"+
                     campingCar.companyId+"')";
-            /* 성공 여부를 반환 */
+            // 성공 여부를 반환 
             int rowCount = statement.executeUpdate(query);
             if (rowCount != 0) {
                 return ResultState.SUCCESS;
             }
         }catch(Exception e1) {
-            /* 데이터 중 하나라도 비어있던 경우 */
+            // 데이터 중 하나라도 비어있던 경우 
             if(campingCar.isNull()) {
-                /* 다른 상황을 알려주는 결과값을 반환 */
+                // 다른 상황을 알려주는 결과값을 반환 
                 return ResultState.NULL;
             }
             System.out.println(e1);
@@ -44,14 +117,17 @@ public class CampingCarModel {
         return ResultState.FAILURE;
     }
 
-    /* 컨트롤러에서 넘겨 준 id를 찾아서 같은 id를 갖는 캠핑카를 반환 */
+    
+    
+    //
+    // 컨트롤러에서 넘겨 준 id를 찾아서 같은 id를 갖는 캠핑카를 반환 
     public CampingCarInfo readCampingCar(String id) {
         CampingCarInfo campingCar = new CampingCarInfo();
         try {
             statement = connection.createStatement();
-            String query=" select * from campingcar_list where campingcar_list_id='"+id+"';"; /* SQL 문 */
+            String query=" select * from campingcar_list where campingcar_list_id='"+id+"';"; // SQL 문 
             resultSet = statement.executeQuery(query);
-            /* 같은 값이 있는 경우 캠핑카 데이터 클래스 형태로 반환 */
+            // 같은 값이 있는 경우 캠핑카 데이터 클래스 형태로 반환 
             if(resultSet.next()) {
                 campingCar = toCampingCarFromResultSet(resultSet);
             }
@@ -61,14 +137,16 @@ public class CampingCarModel {
         return campingCar;
     }
 
-    /* [모델] 현재 등록된 캠핑카들의 리스트를 만들어 반환 */
+    
+    //
+    // [모델] 현재 등록된 캠핑카들의 리스트를 만들어 반환 
     public ArrayList<CampingCarInfo> readCampingCarList() {
         ArrayList<CampingCarInfo> campingCarList = new ArrayList<>();
         try {
             statement = connection.createStatement();
-            String query2 = " select * from campingcar_list"; /* SQL 문 */
+            String query2 = " select * from campingcar_list"; // SQL 문
             resultSet = statement.executeQuery(query2);
-            /* SQL을 통해 가져온 데이터를 데이터 클래스 형태로 재구성 */
+            // SQL을 통해 가져온 데이터를 데이터 클래스 형태로 재구성 
             while (resultSet.next()) {
                 CampingCarInfo campingCar = toCampingCarFromResultSet(resultSet);
                 campingCarList.add(campingCar);
@@ -79,10 +157,13 @@ public class CampingCarModel {
         return campingCarList;
     }
 
+    
+    
+    //
     public ResultState updateCampingCar(CampingCarInfo campingCar) {
         try {
             statement = connection.createStatement();
-            /* 입력받은 데이터를 SQL 형태로 구성 */
+            // 입력받은 데이터를 SQL 형태로 구성 
             String query="update campingcar_list set cc_name='"+campingCar.name+
                     "',cc_number='"+campingCar.number+
                     "',cc_sits='"+campingCar.seats+
@@ -93,13 +174,13 @@ public class CampingCarModel {
                     "',cc_regist_date='"+campingCar.registryDate +
                     "',campingcar_rent_company_id='"+campingCar.companyId+"'" +
                     " where campingcar_list_id='"+campingCar.id+"'";
-            /* 성공 여부를 저장 */
+            // 성공 여부를 저장 
             int rowCount = statement.executeUpdate(query);
             if (rowCount != 0) {
                 return ResultState.SUCCESS;
             }
         }catch(Exception e1) {
-            /* 데이터가 비어있던 경우의 상태 */
+            // 데이터가 비어있던 경우의 상태 
             if(campingCar.isNull()) {
                 return ResultState.NULL;
             }
@@ -108,6 +189,7 @@ public class CampingCarModel {
         return ResultState.FAILURE;
     }
 
+    //
     public ResultState deleteCampingCar(String id) {
         int result1 = 0;
         int result2 = 0;
@@ -120,9 +202,7 @@ public class CampingCarModel {
         } catch (Exception e1) {
             //System.out.println(e1);
         }
-        /* 캠핑카 목록과 대여 중인 캠핑카 목록 둘 다에서 삭제된 경우에만 성공
-        * (!) 그런데 대여 중인 캠핑카를 삭제하면 문제가 될 수 있다.
-        * (!) 대여 중인지를 먼저 파악하고, 대여 중이면 삭제를 거부해야 한다. */
+
         if (result1 == 1 && result2 == 1) {
             return ResultState.SUCCESS;
         } else {
@@ -130,7 +210,7 @@ public class CampingCarModel {
         }
     }
 
-    /* [모델] ResultSet을 캠핑카 데이터 클래스 형태로 바꿔주는 기능 */
+    // [모델] ResultSet을 캠핑카 데이터 클래스 형태로 바꿔주는 기능 
     private CampingCarInfo toCampingCarFromResultSet(ResultSet result) throws Exception {
         CampingCarInfo campingCar = new CampingCarInfo();
         campingCar.id = Integer.toString(result.getInt(1));
@@ -145,4 +225,7 @@ public class CampingCarModel {
         campingCar.companyId = result.getString(10);
         return campingCar;
     }
-}
+    
+    
+    */
+    
