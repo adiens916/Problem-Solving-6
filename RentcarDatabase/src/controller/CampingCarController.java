e5f766923d13edf0a5c2765ff965f922442e8be8
hkmast <50509845+hkmast@users.noreply.github.com>
2020-11-28 17:53:42 +0900

    campingcar controller update

--- RentcarDatabase/src/controller/CampingCarController.java
+++ RentcarDatabase/src/controller/CampingCarController.java
@@ -1,76 +1,18 @@
 package controller;
 
-import java.util.ArrayList;
+import java.awt.EventQueue;
+
 import controller.dataClass.CampingCarInfo;
-import controller.dataClass.ResultState;
 import model.CampingCarModel;
+import view.AdminView;
 import view.CampingCarView;
 
-import javax.swing.*;
+public class CampingCarController extends AbstractController<CampingCarModel, CampingCarView, CampingCarInfo> {
 
-public class CampingCarController {
-
-	private final CampingCarModel campingCarModel = new CampingCarModel();
-	private final CampingCarView campingCarView = new CampingCarView();
-	public final JButton quit = campingCarView.quit;
-
-	public CampingCarController() {
-		readCampingCarList();
-		listenToRefreshInput();
-		listenToCreateCampingCar();
-		listenToReadCampingCar();
-		listenToUpdateCampingCar();
-		listenToDeleteCampingCar();
-	}
-
-	public void setVisible(boolean value) {
-		campingCarView.setVisible(value);
-	}
-
-	private void readCampingCarList() {
-		ArrayList<CampingCarInfo> campingCarList = campingCarModel.readCampingCarList();
-		campingCarView.readCampingCarList(campingCarList);
-	}
-
-	private void listenToRefreshInput() {
-		campingCarView.btn_Refresh.addActionListener(e -> {
-			readCampingCarList();
-			campingCarView.refreshInput();
-		});
-	}
-
-	private void listenToCreateCampingCar() {
-		campingCarView.btn_Insert.addActionListener(e -> {
-			CampingCarInfo campingCar = campingCarView.getCampingCarInput();
-			ResultState result = campingCarModel.createCampingCar(campingCar);
-			campingCarView.showCreateResult(result);
-			readCampingCarList();
-		});
-	}
-
-	private void listenToReadCampingCar() {
-		campingCarView.btn_Search.addActionListener(e -> {
-			String campingCarId = campingCarView.getCampingCarId();
-			CampingCarInfo campingCar = campingCarModel.readCampingCar(campingCarId);
-			campingCarView.readCampingCar(campingCar);
-		});
-	}
-
-	private void listenToUpdateCampingCar() {
-		campingCarView.btn_Edit.addActionListener(e -> {
-			CampingCarInfo campingCar = campingCarView.getCampingCarInput();
-			ResultState result = campingCarModel.updateCampingCar(campingCar);
-			campingCarView.showUpdateResult(result);
-			readCampingCarList();
-		});
-	}
-
-	private void listenToDeleteCampingCar() {
-		campingCarView.btn_Delete.addActionListener(e -> {
-			String campingCarId = campingCarView.getCampingCarId();
-			ResultState result = campingCarModel.deleteCampingCar(campingCarId);
-			campingCarView.showDeleteResult(result);
-			readCampingCarList();
-		});
+	@Override
+	void setModelAndView() {
+		model = new CampingCarModel();
+		view = new CampingCarView();
+		quit = view.quit;
 	}
 }
